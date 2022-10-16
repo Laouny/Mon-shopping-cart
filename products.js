11cbf8e51afaa6e5465f727bbb26d60d86e36838
Ben laouny <benbema06@gmail.com>
2022-10-16 10:46:52 +0000

    mon shopping cart

---
+++ products.js
@@ -0,0 +1,56 @@
+const products = [
+  {
+    id: 0,
+    name: "Amazone Smart Box",
+    price: 29.99,
+    instock: 100,
+    description:
+      "Amazone Smart Box",
+    imgSrc: "./img/img1.jpeg",
+  },
+  {
+    id: 1,
+    name: "Amazon Alexa",
+    price: 24.99,
+    instock: 43,
+    description:
+      "Amazon Alexa",
+    imgSrc: "./img/img2.jpeg",
+  },
+  {
+    id: 2,
+    name: "Sac a main",
+    price: 19.99,
+    instock: 10,
+    description:
+      "Sac a main",
+    imgSrc: "./img/img3.jpeg",
+  },
+  {
+    id: 3,
+    name: "Gant thermique",
+    price: 25.99,
+    instock: 5,
+    description:
+      "Gant Thermique",
+    imgSrc: "./img/img4.jpeg",
+  },
+  {
+    id: 4,
+    name: "Bonnet Noir",
+    price: 29.99,
+    instock: 4,
+    description:
+      "Bonnet Noir ",
+    imgSrc: "./img/img5.png",
+  },
+  {
+    id: 5,
+    name: "Coffee Cup",
+    price: 39.99,
+    instock: 40,
+    description:
+      "Coffee Cup ",
+    imgSrc: "./img/img6.jpeg",
+  },
+];
